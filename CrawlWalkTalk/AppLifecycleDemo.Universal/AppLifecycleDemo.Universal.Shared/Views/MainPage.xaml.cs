de942b974d02309f21442b0e191aae97b3ef3059
Danny Warren <dannydwarren@gmail.com>
2014-11-19 21:03:14 -0800

    Upgraded AppLifecycleDemo to Windows 8.1 and Windows Phone 8.1 Universal App

---
+++ CrawlWalkTalk/AppLifecycleDemo.Universal/AppLifecycleDemo.Universal.Shared/Views/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AppLifecycleDemo.Universal.Views
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		private void PartA_Click(object sender, RoutedEventArgs routedEventArgs)
+		{
+			Navigate("A");
+		}
+
+		private void PartB_Click(object sender, RoutedEventArgs routedEventArgs)
+		{
+			Navigate("B");
+		}
+
+		private void PartC_Click(object sender, RoutedEventArgs routedEventArgs)
+		{
+			Navigate("C");
+		}
+
+		private void PartD_Click(object sender, RoutedEventArgs routedEventArgs)
+		{
+			Navigate("D");
+		}
+
+		private void PartE_Click(object sender, RoutedEventArgs routedEventArgs)
+		{
+			Navigate("E");
+		}
+
+		private void Navigate(string stateParameter)
+		{
+			//TODO: AppLifecycleDemo 1.0 - Navigate
+
+			Frame.Navigate(typeof(ContentPage), stateParameter);
+		}
+	}
+}
